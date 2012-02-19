e7263ec072850ef22306f5b6d9bdcfb088c75467
Yuki Nagato <foxnewsnetwork@gmail.com>
2012-02-18 16:21:02 -0800

    scaffolded out the server app also

--- app.js
+++ app.js
@@ -39,12 +39,86 @@ app.configure('production', function(){
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
-// Socket io stuff
 app.get('/', function(req, res){
 	 // res.sendfile(__dirname + '/index.html');
 	 res.render("index.jade", { title : "beta faggot losers" });
 });
 
+/****************************
+* Socket IO Response Server *
+****************************/
+// Initialize the socket connection
 io.sockets.on('connection', function(socket){
-	socket.emit('news', "faggot" );
+
+	/****************************
+	* Player Server Response        *
+	****************************/
+	// player login
+	socket.on( "player login up", function(playerinfo){ 
+		// TODO: handle and process playerinfo
+		var result;
+		socket.emit( "player login down", result );
+	} );
+	
+	
+	/****************************
+	* Shop Server Response           *
+	****************************/
+	// open shop
+	socket.on( "open shop up", function(metadata){
+		// TODO: handle the api calls for this
+		var items;
+		socket.emit( "open shop down", items );
+	} );
+	
+	// purchase item
+	socket.on( "purchase item up", function(data){
+		// TODO: write this
+		var result;
+		socket.emit( "purchase item down", result )
+	} );
+	
+	
+	/****************************
+	* Channel Server Response       *
+	****************************/
+	// join channel
+	socket.on( "join channel up", function( data ){ 
+		// TODO: write this
+		var result;
+		socket.emit( "join channel down", result );
+	});
+	
+	// chat
+	socket.on( "chat up", function(data){ 
+		// TODO: write this
+		var event;
+		socket.emit( "chat down", event );
+	});
+	
+	
+	/****************************
+	* Game Server Response          *
+	****************************/
+	// join game
+	socket.on( "join game up", function( data ){
+		// TODO: write this
+		var result;
+		socket.emit( "join game down", result );
+	} );
+	
+	// game event
+	socket.on( "game event up", function(data){ 
+		// TODO: write this
+		var event;
+		socket.emit( "game event down", event );
+	});
+	
+	// sync
+	socket.on( "sync up", function( syncRatio ){ 
+		// TODO: write this
+		var syncValue;
+		socket.emit( "sync down", syncValue );	
+	});
+
 });
--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -28,22 +28,44 @@ var playerState = STATE_PREGAME;
 // -1 means we're either not in a game, or error has occured.
 var syncPercentDeviation = -1;
 
-// Hash map to hold event handling functions
-var customFunctionHandlers = {};
+// Hash map to hold event handling game-related functions
+var gameFunctionHandlers = {};
+
+// Hash map for chat related event handling
+var chatFunctionHandlers = {};
+
+
+/**********************
+* Programmer Functions *
+**********************/
+// Adds a function to handle game events
+function AddGameFunction( eventname, func ){ 
+	if( gameFunctionHandlers[ eventname ] === undefined ) { 
+		gameFunctionHandlers[ eventname ] = [];
+	}
+	gameFunctionHandlers[ eventname ].push( func );
+}
+
+// Adds a function handler to chat events
+function AddChatFunction( eventname, func ){ 
+	if( chatFunctionHandlers[ eventname ] === undefined ) { 
+		chatFunctionHandlers[ eventname ] = [];
+	}
+	chatFunctionHandlers[ eventname ].push( func );
+}
 
 /**********************
 * User Functions               *
 **********************/
 // playerinfo is a json 
 function LoginPlayer( playerinfo ){ 
-	socket.emit( "player login", playerinfo , function(token){ 
-		if( result != 0 )
-			playerToken = token;
-		else {
-			// TODO: write code to handle failed logins
-		}
-	} );
+	socket.emit( "player login up", playerinfo ); 
 }
+
+socket.on( "player login down", function(token) { 
+	playerToken = token;
+	// TODO: write code to handle the case when player tokens are false
+});
 
 
 /**********************
@@ -51,18 +73,22 @@ function LoginPlayer( playerinfo ){
 **********************/
 // metadata is not required
 function GetShopItems( metadata ){ 
-	socket.emit( "open shop", metadata, function( items ){ 
-		shopItems = items;
-	} );
+	socket.emit( "open shop up", metadata );
 }
+
+socket.on( "open shop down", function( items ){ 
+	shopItems = items;
+	// TODO: write code to handle bad items or whatever
+} );
 
 // playerToken and item id are required to complete a purchase
 function BuyItem( player, item ){ 
-	socket.emit( "purchase item", { playerToken: player, itemId: item }, function( result ){ 
-		// TODO: write a function to display some sort of message for success or failure
-	} );
+	socket.emit( "purchase item up", { playerToken: player, itemId: item } );
 }
 
+socket.on( "purchase item down", function(result){
+	// TODO: write a function to display some sort of message for success or failure
+} );
 
 /**********************
 * Channel functions         *
@@ -71,11 +97,15 @@ function BuyItem( player, item ){
 // player can be the player token or ip address.
 // if no channel is specified, the server decides where to put the player
 function JoinChannel( player, channel ){ 
-	socket.emit( "join channel", { playerInfo: player, channelId: channel }, function( result ){ 
+	socket.emit( "join channel up", { playerInfo: player, channelId: channel }, function( result ){ 
 		// TODO: write a function to let the player know he has joined a new room
 		playerState = STATE_PREGAME;
 	});
 }
+
+socket.on( "join channel down", function( result ){
+	// TODO: let the player he has not left his previous channel
+} );
 
 /**********************
 * Gameroom functions     *
@@ -84,21 +114,33 @@ function JoinChannel( player, channel ){
 // player can be the token or the ip address.
 // if no roomid is specified, the server puts the player into the a game randomly
 function JoinGame( player, room ){ 
-	socket.emit( "join game", { playerInfo: player, roomId: room }, function( result ){ 
+	socket.emit( "join game up", { playerInfo: player, roomId: room }, function( result ){ 
 		// TODO: write me!
 		playerState = STATE_INGAME;		
 	} );
 }
+
+socket.on( "join game down", function( result ){
+	// TODO: let the player know he has joined a game
+} );
 
 /**********************
 * Chat functions              *
 **********************/
 // if no receiver is specified, the message is delivered to the channel or room the player is in
 function PlayerChat( sender, message, receiver ){ 
-	socket.emit( "chat", { senderId: sender, text: message, receiverId: receiver }, function( result ){ 
+	socket.emit( "chat up", { senderId: sender, text: message, receiverId: receiver }, function( result ){ 
 		// TODO: write me!
 	} );
 }
+
+socket.on( "chat down", function( event ){ 
+	// TODO: Write a function 
+	var handlers = chatFunctionHandlers[event['name']];
+	for( var x in handlers ){ 
+		handlers[x](event['data']);
+	}
+})
 
 /**********************
 * Game functions            *
@@ -107,15 +149,17 @@ function PlayerChat( sender, message, receiver ){
 function UpdateServer( sender, event, receiver ){ 
 	socket.emit( "game event up", { senderId: sender, data: event, receiverId: receiver }, function( result ){ 
 		// TODO: write me!
-		socket.emit( "sync request", syncPercentDeviation, function( value ){ 
-			syncPercentDeviation = value;
-		} );
+		socket.emit( "sync up", syncPercentDeviation);
 	} );
 }
 
+socket.on("sync down", function( value ){ 
+	syncPercentDeviation = value;
+});
+
 socket.on("game event down", function(event){
 	// TODO: handle the null case when there are no event handlers
-	var handlers = customFunctionHandlers[event['name']])
+	var handlers = gameFunctionHandlers[event['name']]
 	for( var x in handlers ){ 
 			handlers[x](event['data']);
 	}		

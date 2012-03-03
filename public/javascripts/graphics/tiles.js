253937f6320b0f2cfb57352ab826a9838b2b4192
foxnewsnetwork <foxnewsnetwork@gmail.com>
2012-03-02 21:50:07 -0800

    used photoshop to add in a shitton of sprites for mahjong tiles so that it can be easily used in gamequery

--- public/javascripts/graphics/tiles.js
+++ public/javascripts/graphics/tiles.js
@@ -4,7 +4,7 @@ function CreateSet(){
 	for(var suit = 0; suit < 5; suit++){ 
 		for(var value = 0; value < 9; value++){ 
 			tileset.push( new $.gameQuery.Animation( { 
-				'imageURL': IMAGE_PATH + "tile" + ( suit * 9 + value ) + ".png"
+				'imageURL': IMAGE_PATH + "tiles/" + ( suit * 10 + value ) + ".png"
 			} ) );
 		}
 	}

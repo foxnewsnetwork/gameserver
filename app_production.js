b83d30dd56ae35613fed09a9c4f6077759f6c617
Yuki Nagato <foxnewsnetwork@gmail.com>
2012-03-21 12:33:12 -0700

    put in the forever demon, got rid of unnecessary packages, and put in a separate file for production class app deployment

--- app_production.js
+++ app_production.js
@@ -57,7 +57,7 @@ app.configure('production', function(){
 
 // Routes
 
-app.listen(3000);
+app.listen(80);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 app.get('/', function(req, res){

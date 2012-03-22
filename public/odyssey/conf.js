2d7c9c28459f344e627ba25630d220643a82079c
Yuki Nagato <foxnewsnetwork@gmail.com>
2012-03-22 09:34:39 -0700

    Fixed up some really messed up shit

---
+++ public/odyssey/conf.js
@@ -0,0 +1,11 @@
+var IMAGE_DIR = 'images/open-odyssey/',
+    GAME_WIDTH = 600,
+    GAME_HEIGHT = 600,
+    Z_BACKGROUND = 1,
+    PLAYER_STEP = 7,
+    Z_SHADOWS = 10,
+    SCROLL_SPEED = 3,
+    NUMBER_OF_OBSTACLES = 3;
+    Z_CHARACTERS = 20,
+    Z_GUI=250,
+    PAUSED = 1;

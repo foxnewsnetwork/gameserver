07e6dc91b9728b135598e5d1c15691d2455beee1
Yuki Nagato <foxnewsnetwork@gmail.com>
2012-03-25 22:10:46 -0700

    put in the a compression code for making compiled javascript from separate files. does not obfuscate, though

---
+++ compress.rb
@@ -0,0 +1,27 @@
+# This ruby file squeezes a bunch of javascript files into 1 javascript file
+files = [
+	'./public/javascripts/jquery/jquery.js' ,
+	'./public/javascripts/gameQuery/gameQuery.js' ,
+	'./public/javascripts/alice/alice.js' ,
+	'./public/javascripts/alice/alice.core.js' ,
+	'./public/javascripts/alice/alice.plugins.cheshire.js' ,
+	'./node_modules/socket.io/node_modules/socket.io-client/dist/socket.io.js',
+	'./public/javascripts/clientv2.js' ,
+	'./public/javascripts/form/form.js' ,
+	'./public/javascripts/shop/conf.js' ,
+	'./public/javascripts/shop/tooltip.js' ,
+	'./public/javascripts/shop/generator.js' ,
+	'./public/javascripts/shop/shop.js' ,
+	'./public/javascripts/shop/game-drive.js' ,
+]
+
+output = File.open( "hypedog.js", "a+" )
+files.each do |filename|
+	file = File.open( filename, "r" )
+	file.each do |line|
+		output.puts( line )
+	end
+	puts file.size.to_s + " added! \n"
+end
+
+puts "Total size: " + output.size.to_s
